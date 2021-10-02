dc20c5542ba723aa089ae685d8f75431ee77ab76
Sundaresan <76504950+SIL-A@users.noreply.github.com>
2021-10-02 23:48:46 +0530

    First commit

---
+++ Filtered_XSS/x.php
@@ -0,0 +1,18 @@
+<?php
+if(!array_key_exists ("name", $_GET) || $_GET['name'] == NULL || $_GET['name'] == ''){
+
+ $isempty = true;
+
+} else {
+ $name = preg_replace( '/<(.*)s(.*)c(.*)r(.*)i(.*)p(.*)t/i', '', $_GET['name'] );
+ if (strpos($name,"alert(FLAG)") !== false) {
+  $x = true;
+}if (strpos ($name, "<img") !== false) {
+  $y = true;
+}
+ if ($x  == true && $y == true){
+  echo "FLAG{GCHJP2FVWIMSZCG4L8W1}";
+}
+}
+
+?>
