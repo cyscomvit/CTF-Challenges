b1352885e753a147a3192c0c2076b57510af1bf2
erbmu <76506991+erbmu@users.noreply.github.com>
2021-10-09 00:37:13 +0530

    Sanitised the input

--- challenges/level_15/index.php
+++ challenges/level_15/index.php
@@ -2,7 +2,19 @@
 include 'index2.html';
 if (isset($_GET['cmd'])){
     $cmd = $_GET['cmd'];
-    echo '<br/><br/>';
-    system($cmd);
+    $commands = array("ls","cat","pwd","cd","whoami");
+    $flag = 0;
+    for($i=0;$i<5;$i++){
+        if(strcmp($commands[$i],$cmd) === 0){ 
+                $flag = 1;
+                }
+        }
+    if($flag === 1){
+        echo '<br/><br/>';
+        system($cmd);
+        }
+    else{
+        echo 'This command is not permitted. Try again with basic commands'; 
+        }
 }
 ?>
