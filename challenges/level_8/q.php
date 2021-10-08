aa4d897d1145dfdf65690ee7a59aaea820982c66
Sundaresan <76504950+SIL-A@users.noreply.github.com>
2021-10-08 18:41:41 +0530

    New php file with basic filtering

---
+++ challenges/level_8/q.php
@@ -0,0 +1,10 @@
+<?php
+ $name = $_GET['name'];
+ $name = strtolower($name);
+ if (strpos($name,"<script>") !== false or strpos($name,"<img") !== false){
+echo "WASP{cOnGrATuLaTioNs_Challenge8_cOmPl3ted!}";
+}else{
+ header('Location: https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpics.me.me%2Fwhen-you-expect-a-meme-but-its-me-dio-kono-47076994.png&f=1&nofb=1');
+}
+?>
+sila
