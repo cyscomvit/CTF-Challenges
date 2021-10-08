0a3fb290fac225c316a6d421fa5f2a4718c9a0db
erbmu <76506991+erbmu@users.noreply.github.com>
2021-10-08 14:48:42 +0530

    Updated Flag

--- challenges/level_11/form.php
+++ challenges/level_11/form.php
@@ -3,7 +3,7 @@ include 'flagsearchneedle.html';
 if (isset($_GET['needle'])){
 	$search = $_GET['needle'];
 	if ($search === 'needle'){
-		echo 'WASP(NeEdLe_HuH?)';
+		echo 'WASP{NeEdLe_HuH?}';
 	}
 	else{
 	 	$v = system("grep -i $search Oxford_list.txt", $result_code);
