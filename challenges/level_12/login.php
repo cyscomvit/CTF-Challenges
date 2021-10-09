31c621c42e352bd29bb8a032ba25eb20b672c332
hemangahuja <76509238+hemangahuja@users.noreply.github.com>
2021-10-09 14:08:33 +0530

    login.php level 12

--- challenges/level_12/login.php
+++ challenges/level_12/login.php
@@ -2,90 +2,97 @@
 
 $username=$_POST['username'];
 $password=$_POST['password'];
-
 /* Attempt MySQL server connection. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-$link = mysqli_connect("localhost", "root", "");
+$link = new mysqli("mydb", "root", "password");
  
 // Check connection
 if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
+    die("ERROR: Could not connect. " . $link->connect_error);
 }
  
 // Attempt create database query execution
-$sql = "CREATE DATABASE if not exists demo";
-if(!mysqli_query($link, $sql)){
-    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+$sql = "CREATE DATABASE if not exists ctf";
+if($link->query($sql)){
+
+} else{
+    echo "ERROR: Could not able to execute $sql. " . $link->error;
 }
 
-$link = mysqli_connect("localhost", "root", "", "demo");
+$link = new mysqli("mydb", "root", "password", "ctf");
  
 // Check connection
 if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
+    die("ERROR: Could not connect. " . $link->connect_error);
 }
-
 
 $sql = "Drop table users;";
+if($link->query($sql)){
 
-if(!mysqli_query($link, $sql)){
-    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+} else{
+    echo "ERROR: Could not able to execute $sql. " . $link->error;
 }
 
-$sql="Drop table owasp";
-if(!mysqli_query($link, $sql)){
-    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+$sql = "Drop table owasp;";
+if($link->query($sql)){
+
+} else{
+    echo "ERROR: Could not able to execute $sql. " . $link->error;
 }
 
-$sql = "CREATE TABLE if not exists users(
+
+
+$sql = "CREATE TABLE users(
     username VARCHAR(30),
     password VARCHAR(30)
 )";
-if(!mysqli_query($link, $sql)){
-    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+
+if($link->query($sql)){
+
+} else{
+    echo "ERROR: Could not able to execute $sql. " . $link->error;
 }
 
-$sql = "CREATE TABLE if not exists owasp(
+$sql="CREATE TABLE owasp(
     flag VARCHAR(30)
 )";
-if(!mysqli_query($link, $sql)){
-    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+
+if($link->query($sql)){
+
+} else{
+    echo "ERROR: Could not able to execute $sql. " . $link->error;
 }
 
+$sql = "INSERT INTO users (username,password) VALUES ('admin','auwdjawoidjawiodj')";
+
+if($link->query($sql)){
+
+} else{
+    echo "ERROR: Could not able to execute $sql. " . $link->error;
+}
 
 $sql = "INSERT INTO owasp VALUES ('WASP{You_ReAcheD_LeveL_13}')";
+if($link->query($sql)){
 
-if(!mysqli_query($link, $sql)){
-    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+} else{
+    echo "ERROR: Could not able to execute $sql. " . $link->error;
 }
-
-
-
-$sql = "INSERT INTO users (username,password) VALUES ('admin','qowkdqwiohqweiopxm')";
-
-if(!mysqli_query($link, $sql)){
-    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
-}
-
-
  
 $sql = "SELECT * FROM users where username='$username' and password='$password' ";
 
+if($result = $link->query($sql) and !preg_match('/\s/',$username)){
 
-
-if($result = mysqli_query($link, $sql) and !preg_match('/\s/',$username)){
-
-    $num=mysqli_num_rows($result);
+    $num=$result->num_rows;
     if($num > 0){
             echo "<table>";
-        while($row = mysqli_fetch_array($result)){
+        while($row = $result->fetch_array()){
             echo "<tr>";
                 echo "<td>" . $row['username'] . "</td>";
                 echo "<td>" . $row['password'] . "</td>";
             echo "</tr>";
         }
         echo "</table>";
-        mysqli_free_result($result);
+        $result->free();
     } else{
         echo "Hello $username Your password is $password";
     }
@@ -95,9 +102,9 @@ if($result = mysqli_query($link, $sql) and !preg_match('/\s/',$username)){
         echo 'username cannot have whitespace';
     }
     else{
-    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+    echo "ERROR: Could not able to execute $sql. " . $link->error;
     }
 }
 // Close connection
-mysqli_close($link);
+$link->close();
 ?>
