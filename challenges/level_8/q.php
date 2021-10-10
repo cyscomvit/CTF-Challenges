4075ab9dad9dfa2fb851202bb5f7aa2ad5270a81
Bastaaditya <Bastaaditya001@gmail.com>
2021-10-10 16:50:43 +0000

    final changes... :<

--- challenges/level_8/q.php
+++ challenges/level_8/q.php
@@ -7,4 +7,4 @@ echo "WASP{cOnGrATuLaTioNs_Challenge8_cOmPl3ted!}";
  header('Location: https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpics.me.me%2Fwhen-you-expect-a-meme-but-its-me-dio-kono-47076994.png&f=1&nofb=1');
 }
 ?>
-sila
+
